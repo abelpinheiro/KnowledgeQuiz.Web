15565d612fd1b066b2265f0d3664c6cb51f98f06
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-06-23 14:16:04 -0300

    fix build dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 
 // https://vite.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    cloudflare()
   ],
   resolve: {
     alias: {
